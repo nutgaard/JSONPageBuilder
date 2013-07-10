00763299c3f87e845599ad571adf2001c8f85767
Nicklas Utgaard <nutgaard@gmail.com>
2013-07-11 00:14:34 +0200

    Refactoring extensions to remove them from global scope

--- js/pagebuilder/extensions/Graph.js
+++ js/pagebuilder/extensions/Graph.js
@@ -1,7 +1,13 @@
-PageBuilder.extensions.graph = function(container, json) {
-    var myDiv = document.createElement('div');
-    PageBuilder.setAttribute(myDiv, 'class', json.classes);
-    PageBuilder.setAttribute(myDiv, 'id', json.id);
-    myDiv.innerHTML = 'This is my graph';
-    container.append(myDiv);
-}
+(function() {
+    var graph = function(container, json) {
+        graph.render(container, json);
+    }
+    graph.render = function(container, json){
+        var myDiv = document.createElement('div');
+        PageBuilder.setAttribute(myDiv, 'class', json.classes);
+        PageBuilder.setAttribute(myDiv, 'id', json.id);
+        myDiv.innerHTML = 'This is my graph';
+        container.append(myDiv);
+    }
+    PageBuilder.extensions.graph = graph;
+})();
--- js/pagebuilder/extensions/PercentileTable.js
+++ js/pagebuilder/extensions/PercentileTable.js
@@ -1,5 +1,73 @@
-PageBuilder.extensions.percentileTable = function(container, json){
-    function calculatePercentile(ydata, percentile){
+(function() {
+    var percentileTable = function(container, json){
+        percentileTable.render(container, json);
+    }
+    percentileTable.render = function(container, json){
+        var table = document.createElement('table');
+        PageBuilder.setAttribute(table, 'class', json.data.tablestyle);
+
+        //create header
+        var thead = document.createElement('thead');
+        PageBuilder.setAttribute(thead, 'class', json.data.tableheaderstyle);
+
+        var tr = document.createElement('tr');
+        PageBuilder.setAttribute(tr, 'class', json.data.tablerowstyle);
+
+        var th = document.createElement('th');
+        th.innerHTML = 'Name/Percentiles';
+        PageBuilder.setAttribute(th, 'class', json.data.tabledatastyle);
+
+        tr.appendChild(th);
+        thead.appendChild(tr);
+        table.appendChild(thead);
+
+        for (var i = 0; i < json.data.percentileValues.length; i++){
+            var th = document.createElement('th');
+            th.innerHTML = json.data.percentileValues[i];
+            PageBuilder.setAttribute(th, 'class', json.data.tabledatastyle);
+
+            tr.appendChild(th);
+        }
+        var tbody = document.createElement('tbody');
+        table.appendChild(tbody);
+
+        //request table data
+        for (var rowId = 0; rowId < json.data.percentileOf.length; rowId++){
+            var rowName = json.data.percentileOf[rowId];
+            var tr = document.createElement('tr');
+            PageBuilder.setAttribute(tr, 'class', json.data.tablerowstyle);
+            PageBuilder.setAttribute(tr, 'data-name', rowName);
+            tbody.appendChild(tr);
+
+            var td = document.createElement('td');
+            td.innerHTML = rowName;
+            PageBuilder.setAttribute(td, 'class', json.data.tablecellstyle);
+            tr.appendChild(td);
+
+            for (var i = 0; i < json.data.percentileValues.length; i++){
+                var td = document.createElement('td');
+                PageBuilder.setAttribute(td, 'class', json.data.tablecellstyle);
+                PageBuilder.setAttribute(td, 'data-percentile', json.data.percentileValues[i]);
+                tr.appendChild(td);
+            }
+            $.get({
+                url: 'data/'+rowName,
+                success: function(resp) {
+                    var resp = JSON.parse(resp);
+                    var tr = $('tr[data-name='+resp.name+']');
+                    var ydata = percentileTable.prepareJSON(resp);
+                    for (var i = 0; i < json.data.percentileValues.length; i++){
+                        var td = tr.find('td[data-percentile="'+json.data.percentileValues[i]+'"]');
+                        var tdData = percentileTable.calculatePercentile(ydata, json.data.percentileValues[i]);
+                        td.html(tdData);
+                    }
+                },
+                fixture:Fixtures.page
+            });
+        }
+        container.html(table.outerHTML);
+    }
+    percentileTable.calculatePercentile = function(ydata, percentile){
         var realInd = percentile/100*(ydata.length-1);
         var ind = Math.round(realInd);
         var frac = realInd-ind;
@@ -15,7 +83,7 @@ PageBuilder.extensions.percentileTable = function(container, json){
         }
         return ans;
     }
-    function prepareJSON(d){
+    percentileTable.prepareJSON = function(d){
         var ykey = d.ykey;
         var data = [];
         for (var i = 0; i < d.data.length; i++){
@@ -24,69 +92,5 @@ PageBuilder.extensions.percentileTable = function(container, json){
         var o = data.sort(function(a,b){return a-b});
         return o;
     }
-    var table = document.createElement('table');
-    PageBuilder.setAttribute(table, 'class', json.data.tablestyle);
-
-    //create header
-    var thead = document.createElement('thead');
-    PageBuilder.setAttribute(thead, 'class', json.data.tableheaderstyle);
-
-    var tr = document.createElement('tr');
-    PageBuilder.setAttribute(tr, 'class', json.data.tablerowstyle);
-
-    var th = document.createElement('th');
-    th.innerHTML = 'Name/Percentiles';
-    PageBuilder.setAttribute(th, 'class', json.data.tabledatastyle);
-
-    tr.appendChild(th);
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    for (var i = 0; i < json.data.percentileValues.length; i++){
-        var th = document.createElement('th');
-        th.innerHTML = json.data.percentileValues[i];
-        PageBuilder.setAttribute(th, 'class', json.data.tabledatastyle);
-
-        tr.appendChild(th);
-    }
-    var tbody = document.createElement('tbody');
-    table.appendChild(tbody);
-
-    //request table data
-    for (var rowId = 0; rowId < json.data.percentileOf.length; rowId++){
-        var rowName = json.data.percentileOf[rowId];
-        var tr = document.createElement('tr');
-        PageBuilder.setAttribute(tr, 'class', json.data.tablerowstyle);
-        PageBuilder.setAttribute(tr, 'data-name', rowName);
-        tbody.appendChild(tr);
-
-        var td = document.createElement('td');
-        td.innerHTML = rowName;
-        PageBuilder.setAttribute(td, 'class', json.data.tablecellstyle);
-        tr.appendChild(td);
-
-        for (var i = 0; i < json.data.percentileValues.length; i++){
-            var td = document.createElement('td');
-            PageBuilder.setAttribute(td, 'class', json.data.tablecellstyle);
-            PageBuilder.setAttribute(td, 'data-percentile', json.data.percentileValues[i]);
-            tr.appendChild(td);
-        }
-        $.get({
-            url: 'data/'+rowName,
-            success: function(resp) {
-                var resp = JSON.parse(resp);
-                var tr = $('tr[data-name='+resp.name+']');
-                var ydata = prepareJSON(resp);
-                for (var i = 0; i < json.data.percentileValues.length; i++){
-                    var td = tr.find('td[data-percentile="'+json.data.percentileValues[i]+'"]');
-                    var tdData = calculatePercentile(ydata, json.data.percentileValues[i]);
-                    td.html(tdData);
-                }
-            },
-            fixture:Fixtures.page
-        });
-    }
-    container.html(table.outerHTML);
-
-
-}
+    PageBuilder.extensions.percentileTable = percentileTable;
+})();
