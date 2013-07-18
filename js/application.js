bc5e23f6c737a9dc89d3b5e59a19b1a1b948944c
Nicklas Utgaard <xnsu@steria.no>
2013-07-18 15:37:22 +0200

    Fixed navbar not showing correct active li bug

--- js/application.js
+++ js/application.js
@@ -1,3 +1,8 @@
+if (typeof String.prototype.startsWith !== 'function') {
+  String.prototype.startsWith = function (str){
+    return this.slice(0, str.length) === str;
+  };
+}
 $(document).ready(function() {
     var AppRouter = Backbone.Router.extend({
         routes: {
@@ -12,19 +17,27 @@ $(document).ready(function() {
             loadPage('last24h', '.applicationcontainer', true);
         }
     });
-    loadPage('navbar', 'body');
+    loadPage('navbar', 'body', false, function() {
+        var containerpage = Backbone.history.location.hash;
+        if (containerpage.length >= 0 && containerpage.startsWith('#page')) {
+             containerpage = containerpage.split('/')[1];
+        }
+        $('.navbar').find('a[href=#'+containerpage+']').parent().click();
+        
+    });
     var router = new AppRouter();
     Backbone.history.start();
-    
+
     $(document).on('navbar.updated', function(event, element) {
         var uri = $(element).children().first().attr('href').slice(1);
-        router.navigate('page/'+uri, {trigger: true});
+        router.navigate('page/' + uri, {trigger: true});
     });
-    
-    
-    function loadPage(page, elementSelector, clear) {
+
+
+    function loadPage(page, elementSelector, clear, callback) {
         var clear = clear || false;
-        if (clear){
+        var callback = callback || function(){};
+        if (clear) {
             $(elementSelector).html('');
         }
         $.get({
@@ -32,6 +45,7 @@ $(document).ready(function() {
             success: function(r) {
                 $('body').trigger('destroy_view');
                 new PageView({model: new PageComponentCollection(JSON.parse(r)), el: elementSelector});
+                callback();
             }
         });
     }
