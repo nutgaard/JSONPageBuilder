f8bc92bf802d53654ba6420a4888c19c473a1e69
ohelstro <Orjan.Helstrom@gmail.com>
2013-07-22 14:11:09 +0200

    Re-added fixtures

--- js/helpers/ajax.js
+++ js/helpers/ajax.js
@@ -1,8 +1,8 @@
 $.get = function(request) {
-    var req = $.extend({type: 'GET'}, request);
+    var req = $.extend({type: 'GET',fixture:Fixtures.page}, request);
     return $.ajax(req);
 }
 $.post = function(request) {
-    var req = $.extend({type: 'POST'}, request);
+    var req = $.extend({type: 'POST',fixture:Fixtures.page}, request);
     return $.ajax(req);
 }
